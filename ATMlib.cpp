f8712d6cccd2609e7d12cd582e7da3028b5e22be
Joeri Gantois <info@JO3RI.be>
2015-09-17 11:33:27 +0200

    start file

---
+++ ATMlib.cpp
@@ -0,0 +1,7 @@
+// Arduboy Tracker Music Library for Arduino Leonardo "Arduboy"
+
+// 2015
+// TEAM a.r.g.
+// Davey Taylor 
+
+#include "ATMlib.h"
---
+++ ATMlib.h
@@ -0,0 +1,9 @@
+// Arduboy Tracker Music Library for Arduino Leonardo "Arduboy"
+
+// 2015
+// TEAM a.r.g.
+// Davey Taylor
+
+#ifndef _ATM_H_
+#define _ATM_H_
+#include "Arduino.h"
