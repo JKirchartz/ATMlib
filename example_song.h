5c76a8c204c9b59a62ae8d2a1ef1e384f9cfb00d
Joeri Gantois <info@JO3RI.be>
2016-02-19 17:04:58 +0100

    some work on the song

--- example_song.h
+++ example_song.h
@@ -1,4 +1,26 @@
 #ifndef EXAMPLE_SONG_H
 #define EXAMPLE_SONG_H
 
+const unsigned char PROGMEM exampleSong[] =
+{
+  // Track Table
+  4, 										//Number of tracks in the file/array
+
+  0,										//Location in the file/array for track 1
+  0,										//Location in the file/array for track 2
+  0,										//Location in the file/array for track 3
+  0,										//Location in the file/array for track 4
+
+  // Channel entry Tracks
+  0,										//Starting track index for channel 0
+  0,										//Starting track index for channel 1
+  0,										//Starting track index for channel 2
+  0,										//Starting track index for channel 3
+
+  // TracK 0
+
+
+
+};
+
 #endif
