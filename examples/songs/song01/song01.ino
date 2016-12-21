fe2de26d0ada27a78468f86f34bcc9bd8e89d999
Joeri Gantois <info@JO3RI.be>
2016-12-21 08:43:12 +0100

    Moved all examples from ARGlib to Arduboy2 lib

--- examples/songs/song01/song01.ino
+++ examples/songs/song01/song01.ino
@@ -1,33 +1,32 @@
 #include <Arduino.h>
+#include <Arduboy2.h>
 #include <ATMlib.h>
-#include "Arglib.h"
 #include "bitmaps.h"
 #include "song.h"
 
 
-Arduboy arduboy;
-Sprites sprites(arduboy);
+Arduboy2Base arduboy;
+Sprites sprites;
 ATMsynth ATM;
 
 void setup() {
-  arduboy.start();
+  arduboy.begin();
   // set the framerate of the game at 60 fps
   arduboy.setFrameRate(60);
-    // Begin playback of song.
-    // Initializes ATMSynth and samplerate
-    // Lower the tempo ever so slightly
-    ATM.play(music);
+  // let's make sure the sound was not muted in a previous sketch
+  arduboy.audio.on();
+  // Initializes ATMSynth and samplerate
+  // Begin playback of song.
+  ATM.play(music);
 }
 
 void loop() {
 
   if (!(arduboy.nextFrame())) return;
-  arduboy.poll();
-  arduboy.clearDisplay();
-  for (byte i = 0; i < 4; i++) sprites.drawSelfMasked(32 * i, 10, TEAMarg, i);
-  sprites.drawSelfMasked(43, 50, TEAM_argPart5, 0);
-  if (arduboy.justPressed(B_BUTTON)) ATM.play(music);
-  if (arduboy.justPressed(A_BUTTON)) ATM.stop();
+  arduboy.pollButtons();
+  arduboy.clear();
+  sprites.drawSelfMasked(34, 4, T_arg, 0);
+  if (arduboy.justPressed(B_BUTTON)) ATM.playPause();
+  if (arduboy.justPressed(A_BUTTON)) ATM.playPause();
   arduboy.display();
 }
-
